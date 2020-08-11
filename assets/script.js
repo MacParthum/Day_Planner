d9d491d45f1309b07c7ce84f14d492860b742410
Mac Parthum <macparthum@gmail.com>
2020-08-11 15:09:11 -0400

    created a row and then first time column

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,39 @@
 function getLocalStorage(key) {
     let value = localStorage.getItem(key);
     if (value) {
-        $('#text${key}).text(value');
+        $(`#text${key}`).text(value);
     }
 }
 
 $(document).ready(function () {
     $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
-     {
-         getLocalStorage(i);
-     }});
+    
+    for (let i = 9; i < 18; i++) {
+
+        // create a row
+        var row = $(`<div data-time=${i} id='${i}' class="row">`);
+
+        // Create the time column
+        var colOne = $('<div class="col-sm-2"> <p class="hour">' + formatTime(i) + '</p>');
+
+        //create column 2
+
+        // append col to row
+        row.append(colOne);
+
+        // last step add rows to container
+        $(".container").append(row);
+
+        getLocalStorage(i);
+    }
+    // sets # hours in the column 9am 10am .....
+    function formatTime(hours) {
+        var amPm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        return hours + amPm;
+    }
+    formatTime();
+
+
+});
