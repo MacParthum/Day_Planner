4227206c8c7e676326053f1086c605ef15710066
Mac Parthum <macparthum@gmail.com>
2020-08-11 19:02:13 -0400

    reworked with Max's help putting all the divs into the html page and letting the js page do the work

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,17 @@
-function getLocalStorage(key) {
-    let value = localStorage.getItem(key);
-    if (value) {
-        $(`#text${key}`).text(value);
+
+
+var currentHour = moment().format('H')
+var currentHour = 12
+
+for (let i = 9; i < 18; i++) {
+    if (currentHour > i) {
+        $("#hour-" + i).addClass("past")
     }
+
 }
-
-$(document).ready(function () {
-    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
-    for (let i = 9; i < 18; i++) {
-
-        // create a row
-        var row = $(`<div data-time=${i} id='${i}' class="row">`);
-
-        // Create the time column
-        var colOne = $('<div class="col-sm-2"> <p class="hour">' + formatTime(i) + '</p>');
-
-        // Created second column textarea 
-        var colTwo = $('<div class="col-sm-8 present"><textarea id=text${i} class="description" placeholder="Write here"></textarea>');
-
-        //create column 3
-        var colThree = $('<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>');
-
-        // append col to row
-        row.append(colOne);
-        row.append(colTwo);
-        row.append(colThree);
-        // Adds rows to the container
-        $(".container").append(row);
-
-        getLocalStorage(i);
-    }
-          // sets hours in first row
-    function formatTime(hours) {
-        var amPm = hours >= 12 ? 'pm' : 'am';
-        hours = hours % 12;
-        hours = hours ? hours : 12;
-        return hours + amPm;
-    }
-    formatTime();
+$(".saveBtn").on("click", function () {
+    var clickedHour = $(this).val()
+    console.log($("#hour-" + clickedHour).val())
 
 
-});
+})
